d65db58ec47dd21ce5af414741b8597fe6c1f185
Aiden Shaevitz <aidenashaevitz@gmail.com>
2022-04-13 15:23:26 -0700

    added test data functionality

--- hw1/hw1-data/hw1_knn.py
+++ hw1/hw1-data/hw1_knn.py
@@ -3,6 +3,7 @@
 import numpy as np
 import sys
 
+
 def scale_age(age):
     a = 2 / (73)
     b = -a * 17
@@ -37,17 +38,21 @@ def binarize(data, mapping, numerical_features=[0,7]):
     outputs = []
     for i, row in enumerate(data):
         for j, x in enumerate(row):
-            if j == 0: # Skip the age, hours worked, and maybe the pos/neg outcome
-                binarized_features[i, j] = scale_age(float(x))
-            elif j == 7:
-                binarized_features[i, j] = scale_hours(float(x))
-            elif j == 9:
-                outputs.append(1 if x==">50K" else -1)
-            else:
-                try:
-                    binarized_features[i][mapping[j, x]] = 1
-                except KeyError:
-                    pass
+            try:
+                if j == 0: # Skip the age, hours worked, and maybe the pos/neg outcome
+                    binarized_features[i, j] = scale_age(float(x))
+                elif j == 7:
+                    binarized_features[i, j] = scale_hours(float(x))
+                elif j == 9:
+                    outputs.append(1 if x==">50K" else -1)
+                else:
+                    try:
+                        binarized_features[i][mapping[j, x]] = 1
+                    except KeyError:
+                        pass
+            except IndexError: # catches case where data doesn't have the 10th(9th) element, namely the training data
+                print(j)
+                pass
     return binarized_features, outputs
 
 def knn(example_feature, train_features, train_output, k=3, o=2):
@@ -72,34 +77,51 @@ def knn(example_feature, train_features, train_output, k=3, o=2):
 def knn_eval(example_features, example_output, train_features, train_output, k=3, o=2):
     error = 0
     positive = 0
+    predictions = []
     for i, feature in enumerate(example_features):
         prediction = knn(feature, train_features, train_output, k, o)
         if prediction != example_output[i]:
             error += 1
         if prediction == 1:
             positive += 1
-
+        predictions.append(prediction)
     percent_error = error / len(example_output) * 100
     percent_positive = positive / len(example_output) * 100
-    return percent_error, percent_positive
+    return percent_error, percent_positive, predictions
     
 
-
-
 if __name__ == "__main__":
-        # Open text file    
+    # Open text file    
     lines_train = open('income.train.txt.5k').readlines()
     data_train = [line.strip().split(", ") for line in lines_train]
 
     lines_dev = open('income.dev.txt').readlines()
     data_dev = [line.strip().split(", ") for line in lines_dev]
-    
-    
+
+    lines_test = open('income.test.blind').readlines()
+    data_test = [line.strip().split(", ") for line in lines_test]
+
+    # Create binary mapping from training data
     bin_map = make_binary_mapping(data_train)
+    # Gets binary features from datasets
     bindata_dev, outputs_dev = binarize(data_dev, bin_map)
     bindata_train, outputs_train = binarize(data_train, bin_map)
+    bindata_test, outputs_test = binarize(data_test, bin_map) # outputs empty list for test set
+    outputs_test = [i for i in range(bindata_test.shape[0])] # Just to get code to run
+    
+    error, pos, predictions = knn_eval(bindata_test, outputs_test, bindata_train, outputs_train, k=9, o=2)
 
-    print(knn_eval(bindata_dev, outputs_dev, bindata_train, outputs_train, k=1, o=2))
+    for i, p in enumerate(predictions):
+        label = ">50K" if p==1 else "<=50K" #
+        print(", ".join(data_test[i] + [label])) # output 10 fields, separated by ", "
+    
+    # ks = [1,3,5,7,9,99,999,9999]
+    # for k in ks:
+    #     error, pos, predictions = knn_eval(bindata_dev, outputs_dev, bindata_train, outputs_train, k=k, o=2)
+    #     print("k={0}    dev_error {1}% (+:{2}% )".format(k, round(error,1),round(pos,1)))
+    
+
+
     # # Get closed people by euclidean and manhattan distance
     # last_person_dev = bindata_dev[-1,:]
     # print(data_dev[-1])
